bb087a40e9d8189593ef513b780413903081f6e8
Sushant <clashofgames229@gmail.com>
2024-07-16 23:32:29 +0545

    making app ready for production

---
+++ app/meals/share/error.js
@@ -0,0 +1,9 @@
+"use client";
+export default function Error() {
+  return (
+    <main className="error">
+      <h1>An error occured!</h1>
+      <p>Failed to create a meal.</p>
+    </main>
+  );
+}
--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,7 +1,12 @@
+"use client";
 import ImagePicker from "@/component/meals/image-picker";
 import classes from "./page.module.css";
 import { shareMealForm } from "@/lib/actions";
+import MealsFormSubmit from "@/component/meals/meals-form-submit";
+import { useFormState } from "react-dom";
+
 export default function ShareMealPage() {
+  const [state, formAction] = useFormState(shareMealForm, {});
   return (
     <>
       <header className={classes.header}>
@@ -11,7 +16,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMealForm}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -40,8 +45,10 @@ export default function ShareMealPage() {
             ></textarea>
           </p>
           <ImagePicker label="your Image" name="image" />
+
+          <p>{state.message && <span>{state.message}</span>}</p>
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
